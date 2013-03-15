9aba67c6594d872a198f0e028be2ef280fa4f266
marpl136 <marpl136@student.liu.se>
2013-03-15 20:53:02 +0100

    Default data Inserts.

--- Forum/war/creation_of_tables.sql
+++ Forum/war/creation_of_tables.sql
@@ -32,3 +32,36 @@ CREATE TABLE messages(
 	parent_thread_id INT NOT NULL,
 	author_username VARCHAR(30) NOT NULL
 );
+
+
+INSERT INTO users values("ADMIN", "admin@gmail.com", "ad_pass", true);
+INSERT INTO users values("Fran", "fran@gmail.com", "fran_pass", false);
+INSERT INTO users values("Maria", "maria@gmail.com", "maria_pass", false);
+INSERT INTO users values("Joham", "joham@gmail.com", "joham_pass", false);
+INSERT INTO users values("Emma", "emma@gmail.com", "emma_pass", false);
+
+
+INSERT INTO topics(name) values("University");
+INSERT INTO topics(name) values("Movies");
+INSERT INTO topics(name) values("Books");
+INSERT INTO topics(name) values("Videogames");
+INSERT INTO topics(name) values("Outdoors");
+INSERT INTO topics(name) values("Politics");
+
+INSERT INTO threads(name, parent_topic_id) values ("Courses to choose", 1);
+INSERT INTO threads(name, parent_topic_id) values ("Erasmus", 1);
+INSERT INTO threads(name, parent_topic_id) values ("Hunger games", 3);
+INSERT INTO threads(name, parent_topic_id) values ("Life of Pi", 2);
+INSERT INTO threads(name, parent_topic_id) values ("Batman", 2);
+
+INSERT INTO messages(content, parent_thread_id, author_username) values ("You should choose TDDD24", 1, "Maria");
+INSERT INTO messages(content, parent_thread_id, author_username) values ("Really? Why?", 1, "Joham");
+INSERT INTO messages(content, parent_thread_id, author_username) values ("Great experience", 2, "Emma");
+INSERT INTO messages(content, parent_thread_id, author_username) values ("Great Movie", 5, "Maria");
+INSERT INTO messages(content, parent_thread_id, author_username) values ("Is not true! First one was the first", 5, "Fran");
+
+
+
+
+
+
